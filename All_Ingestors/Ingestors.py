758b5580f4bcfab647d9ab257a776ac9e4dec8c1
maaxxxx22a <111069500+maaxxxx22a@users.noreply.github.com>
2022-09-08 17:03:21 -0600

    Add files via upload

--- All_Ingestors/Ingestors.py
+++ All_Ingestors/Ingestors.py
@@ -1,3 +1,4 @@
+"""Classes that will read the quotes from the different file types."""
 import pandas as pd
 from QuoteEngine import QuoteModel
 from docx import Document
@@ -6,87 +7,102 @@ import os
 import subprocess
 import csv
 import pdfplumber
+import tempfile
 
 
+file_ext = {
+    "DOCX": ".docx",
+    "PDF": ".pdf",
+    "CSV": ".csv",
+    "TXT": ".txt",
+}
 
-file_ext = {"DOCX": ".docx","PDF": ".pdf","CSV": ".csv","TXT": ".txt",}
 
 class IngestorInterface(ABC):
-    """An abstract base class that is implemented to the IngestorInterface. This class defines two methods, one of
-    which returns a boolean value and the other returns a list.
+    """An abstract base class that is implemented to the IngestorInterface.
+
+    This class defines two methods, one of which returns a boolean value and
+    the other returns a list.
     """
+
     @classmethod
     def verify(cls, path):
-        """File extentions are checked to see if it matches. Returns boolean value."""
+        """File extentions matches are checked and returns boolean value."""
         return path in file_ext.values()
 
     @abstractmethod
     def parse(cls, path):
-        """Returns a Quotemodels list for each quote found in the parsed files."""
+        """Return a Quotemodels list for each quote found."""
         pass
 
 
 class DocxIngestor(IngestorInterface):
-    """The docx file ingestion is handled by this class. This class utilizes the IngestorInterface in order to parse
-     through the docx file to return a list.
-     """
+    """The docx file ingestion is handled by this class."""
+
     @classmethod
     def parse(cls, path):
-        """Returns a list from parsed docx files."""
+        """Return a list from parsed docx files."""
         doc = Document(path)
         quotes = []
         quotes = [QuoteModel(*i.text.split(" - ")) for i in doc.paragraphs]
-        
+
         return quotes
 
 
 class PDFIngestor(IngestorInterface):
-    """The pdf file ingestion is handled by this class. This class utilizes the IngestorInterface in order to parse
-         through the pdf file to return a list."""
+    """The pdf file ingestion is handled by this class."""
+
     @classmethod
     def parse(cls, path):
-        """The pdfplumber tool is used to extract text from the pdf that we can parse and return a list."""
-        with pdfplumber.open(path) as pdf2:
-            pdf_text = pdf2.pages[0].extract_text()
-            text_list = pdf_text.split('\n')[: -3 or None]
-            quotes = [QuoteModel(*i.rstrip("\n").split(" - ")) for i in text_list]
-        return quotes  
+        """Return a list from extracted pdf files."""
+        temp1 = './pdf_text.txt'
+        with open('pdf_text.txt', 'w') as fp:
+            pass
+
+        cmd = f"pdftotext.exe -layout -nopgbrk {path} {temp1}"
+        subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT)
+        quotes = TextIngestor.parse(temp1)
+        os.remove(temp1)
+        return quotes
+
 
 class CSVIngestor(IngestorInterface):
-    """The csv file ingestion is handled by this class. This class utilizes the IngestorInterface in order to parse
-    through the csv file to return a list.
-    """
+    """The csv file ingestion is handled by this class."""
+
     @classmethod
     def parse(cls, path):
-        """Returns a list from parsed csv files."""
+        """Return a list from parsed csv files."""
         with open(path) as csvfile:
-            csv_1={}
+            csv_1 = {}
             csv_1 = csv.DictReader(csvfile)
             quotes = [QuoteModel(**r) for r in csv_1]
         return quotes
 
+
 class TextIngestor(IngestorInterface):
-    """The text file ingestion is handled by this class. This class utilizes the IngestorInterface in order to parse
-    through the text file to return a list.
-    """
+    """The text file ingestion is handled by this class."""
+
     @classmethod
     def parse(cls, path):
-        """Returns a list from parsed text files."""
+        """Return a list from parsed text files."""
         with open(path, "r", encoding="utf-8-sig") as f:
             lines = f.readlines()
         return [QuoteModel(*i.rstrip("\n").split(" - ")) for i in lines]
 
 
-
-
 class Ingestor(IngestorInterface):
-    """The Ingestor class encapsulates the helper classes. It implements logic to select the appropriate helper class
-    for the given file, based on filetype.
-    """
-    ingestors = {DocxIngestor:".docx", PDFIngestor:".pdf", CSVIngestor:".csv", TextIngestor:".txt"}
+    """Encapsulates and selects the appropriate helper class for given file."""
+
+    ingestors = {
+        DocxIngestor: ".docx",
+        PDFIngestor: ".pdf",
+        CSVIngestor: ".csv",
+        TextIngestor: ".txt",
+    }
+
     @classmethod
     def parse(cls, path: str):
-        """This will go through the defined ingestors and selects the appropriate ingestor method."""
+        """Goes through ingestors and selects the appropriate method."""
         filename, file_ext = os.path.splitext(path)
         if not cls.verify(file_ext):
             raise ValueError("Unsupported file extension:", file_ext)
@@ -95,5 +111,3 @@ class Ingestor(IngestorInterface):
                 return i.parse(path)
             else:
                 continue
-
-
