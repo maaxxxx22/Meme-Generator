fce643b26827dbbf971b123aabc520095c165fd6
maaxxxx22a <111069500+maaxxxx22a@users.noreply.github.com>
2022-09-08 17:04:55 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ import os
 import requests
 from itertools import chain
 from flask import Flask, render_template, abort, request
-# @TODO Import your Ingestor and MemeEngine classes
 from meme import MemeEngine
 from All_Ingestors.Ingestors import Ingestor
 
 
 app = Flask(__name__)
-meme = MemeEngine('./static')
+meme = MemeEngine("./static")
 
 
 def setup():
-    """ Load all resources """
-
-    quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
-                   './_data/DogQuotes/DogQuotesDOCX.docx',
-                   './_data/DogQuotes/DogQuotesPDF.pdf',
-                   './_data/DogQuotes/DogQuotesCSV.csv']
+    """Load all resources."""
+    quote_files = [
+        "./_data/DogQuotes/DogQuotesTXT.txt",
+        "./_data/DogQuotes/DogQuotesDOCX.docx",
+        "./_data/DogQuotes/DogQuotesPDF.pdf",
+        "./_data/DogQuotes/DogQuotesCSV.csv",
+    ]
 
     all_quotes = list(chain(*[Ingestor.parse(f) for f in quote_files]))
 
@@ -33,30 +33,30 @@ def setup():
 all_quotes, all_images = setup()
 
 
-@app.route('/')
+@app.route("/")
 def meme_rand():
-    """ Generate a random meme """
+    """Generate a random meme."""
     img = random.choice(all_images)
     quote = random.choice(all_quotes)
     path = meme.make_meme(img, quote.body, quote.author)
     return render_template("meme.html", path=path)
 
 
-@app.route('/create', methods=['GET'])
+@app.route("/create", methods=["GET"])
 def meme_form():
-    """ User input for meme information """
-    return render_template('meme_form.html')
+    """User input for meme information."""
+    return render_template("meme_form.html")
 
 
 @app.errorhandler(Exception)
 def http_error_handler(error):
-    """This is handles invalid url and returns error page."""
-    return flask.render_template('error.html', error=error), error.code
+    """Handle invalid url and returns error page."""
+    return flask.render_template("error.html", error=error), error.code
 
 
-@app.route('/create', methods=['POST'])
+@app.route("/create", methods=["POST"])
 def meme_post():
-    """ Create a user defined meme """
+    """Create a user defined meme."""
     img_temp = "./temp_image.jpg"
 
     """Request is used to save the image from the image_url."""
@@ -64,16 +64,17 @@ def meme_post():
     body = request.form.get("body", "")
     author = request.form.get("author", "")
 
-    img_data = requests.get(image_url, stream=True, allow_redirects=True).content
+    img_data = requests.get(
+        image_url, stream=True, allow_redirects=True
+        ).content
     h = requests.head(image_url, allow_redirects=True)
     header = h.headers
-    content_type = header.get('content-type')
+    content_type = header.get("content-type")
 
     """Arguments to be mapped to the make_meme parameters."""
-    m_args = {'img_path': img_temp, 'text': body, 'author': author}
-    
-    if 'image' in content_type.lower():
-        
+    m_args = {"img_path": img_temp, "text": body, "author": author}
+
+    if "image" in content_type.lower():
 
         with open(img_temp, "wb") as f:
             f.write(img_data)
@@ -83,8 +84,10 @@ def meme_post():
         os.remove(img_temp)
         return render_template("meme.html", path=path)
     else:
-        return render_template('meme_form.html',
-                               error_message='Url does not point to a valid image. Try again.')
+        return render_template(
+            "error.html",
+            error_message="Url does not point to a valid image. Try again.",
+        )
 
 
 if __name__ == "__main__":
